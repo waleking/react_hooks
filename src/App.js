ae7ad13d63ee16fa782619d111206c8c03a620e9
waleking <huangwaleking@gmail.com>
2021-09-26 22:59:42 -0400

    use react-router-dom to switch on different react hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import StateTutorial from "./componets/StateTutorial";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <StateTutorial />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={["/","/useState"]}>
+          <StateTutorial />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
