0c6daf0f548dc24a1e80627b7555837324475b8d
waleking <huangwaleking@gmail.com>
2021-09-27 00:11:26 -0400

    add hyperlinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import StateTutorial from "./componets/StateTutorial";
 import ReducerTutorial from "./componets/ReducerTutorial";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const links = [
+    {
+      text: "useState",
+      hyperlink: "/useState",
+    },
+    {
+      text: "useReducer",
+      hyperlink: "/useReducer",
+    },
+  ];
+  const Links = () =>
+    links.map((link) => <Link to={link.hyperlink}>{link.text}</Link>);
+
   return (
     <BrowserRouter>
+      {/* The comment part is similar as Links*/}
+      {/* {links.map((link) => ( */}
+      {/*   <Link to={link.hyperlink}>{link.text}</Link> */}
+      {/* ))} */}
+      <Links />
       <Switch>
         <Route exact path={["/", "/useState"]}>
           <StateTutorial />
