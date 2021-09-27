80652d4533c351c47f50f36a686b85db3787cded
waleking <huangwaleking@gmail.com>
2021-09-26 23:49:00 -0400

    add a logic which needs to be replaced by useReducer

--- src/componets/ReducerTutorial.js
+++ src/componets/ReducerTutorial.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
+// TODO: we need to change the following logic to useReducer
 const ReducerTutorial = () => {
-  return <div>reducer</div>;
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          setShowText(!showText);
+        }}
+      >
+        click
+      </button>
+      {showText && <p>this is an even number</p>}
+    </div>
+  );
 };
 
 export default ReducerTutorial;
